6f76419bbd9193e2bb7c327b2c36c5bd5154a2f5
Pavle Janjusevic <devredhat@gmail.com>
2023-06-05 17:00:58 +0200

    Generate 4 builds based on the lists

--- api/src/controllers/BuildController.ts
+++ api/src/controllers/BuildController.ts
@@ -6,6 +6,8 @@ const BuildGenerate = async (req: any, res: any) => {
 
     const { budget, purpose } = req.body
 
+    try {
+    
     /* Calculate basis for components prices*/
     const build_prices = getBuildPrices(budget, purpose)
 
@@ -13,13 +15,50 @@ const BuildGenerate = async (req: any, res: any) => {
     const component_lists = generateComponentList(build_prices)
 
 
-    res.status(200).send({
-        build_1: {
-            cpu: '',
-            gpu: '',
-
-        }
-    })
+        res.status(200).send({
+            build_1: {
+                cpu: component_lists.cpu[0],
+                gpu: component_lists.gpu[0],
+                mobo: component_lists.mobo[0],
+                ram: component_lists.ram[0],
+                psu: component_lists.psu[0],
+                ssd: component_lists.ssd[0],
+                case: component_lists.case[0]
+            },
+            
+            build_2: {
+                cpu: component_lists.cpu[1],
+                gpu: component_lists.gpu[1],
+                mobo: component_lists.mobo[1],
+                ram: component_lists.ram[1],
+                psu: component_lists.psu[1],
+                ssd: component_lists.ssd[1],
+                case: component_lists.case[1]
+            },
+            
+            build_3: {
+                cpu: component_lists.cpu[1],
+                gpu: component_lists.gpu[1],
+                mobo: component_lists.mobo[1],
+                ram: component_lists.ram[1],
+                psu: component_lists.psu[1],
+                ssd: component_lists.ssd[1],
+                case: component_lists.case[1]
+            },
+            
+            build_4:  {
+                cpu: component_lists.cpu[1],
+                gpu: component_lists.gpu[1],
+                mobo: component_lists.mobo[1],
+                ram: component_lists.ram[1],
+                psu: component_lists.psu[1],
+                ssd: component_lists.ssd[1],
+                case: component_lists.case[1]
+            }
+        })
+    } catch (err) {
+        throw new Error(err)
+    }
     
     
 
